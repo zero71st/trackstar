f03b0b82c5b5d27bb9492e03daebdbbe1393ee96
Mis02 <Mis02@Mis02>
2013-07-10 15:59:51 +0700

    Completely migrate edit rbac tables, create RbacCommand, add authManager in console.php

--- protected/config/console.php
+++ protected/config/console.php
@@ -3,35 +3,41 @@
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
-
-	// preloading 'log' component
-	'preload'=>array('log'),
-
-	// application components
-	'components'=>array(
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'My Console Application',
+    // preloading 'log' component
+    'preload' => array('log'),
+    // application components
+    'components' => array(
 //		'db'=>array(
 //			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 //		),
-		// uncomment the following to use a MySQL database
-		
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=db_trackstar',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-			),
-		),
-	),
+        // uncomment the following to use a MySQL database
+
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=db_trackstar',
+            'emulatePrepare' => true,
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+            ),
+        ),
+        
+        // authManager เพิ่มเข้ามาเหมือนกับอยู่ที่ Main
+        'authManager' => array(
+            'class' => 'CDbAuthManager',
+            'connectionID' => 'db',
+            'itemTable' => 'tbl_auth_item',
+            'itemChildTable' => 'tbl_auth_item_child',
+            'assignmentTable' => 'tbl_auth_assignment',
+        ),
+    ),
 );
---
+++ protected/migrations/m130710_085319_create_rbac_tables.php
@@ -0,0 +1,66 @@
+<?php
+
+class m130710_085319_create_rbac_tables extends CDbMigration {
+
+    public function up() {
+        //Create the auth item table
+        $this->createTable('tbl_auth_item', array(
+            'name' => 'varchar(64) NOT NULL',
+            'type' => 'Integer NOT NULL',
+            'description' => 'Text',
+            'bizrule' => 'Text',
+            'data' => 'Text',
+            'PRIMARY KEY(name)',
+                ), 'ENGINE=InnoDB');
+
+        //Create the auth child item table
+        $this->createTable('tbl_auth_item_child', array(
+            'parent' => 'varchar(64) NOT NULL',
+            'child' => 'varchar(64) NOT NULL',
+            'PRIMARY KEY(parent,child)',
+                ), 'ENGINE=InnoDB');
+
+        // tbl_auth_item_child.parent is a referrence to tbl_auth_item.name
+        $this->addForeignkey('fk_auth_item_child_parent', 'tbl_auth_item_child', 'parent', 'tbl_auth_item', 'name', 'CASCADE', 'CASCADE');
+
+        // tbl_auth_item_child.child is a referrence to tbl_auth_item.name
+        $this->addForeignkey('fk_auth_item_child_child', 'tbl_auth_item_child', 'child', 'tbl_auth_item', 'name', 'CASCADE', 'CASCADE');
+
+        //Create the auth assignment table
+        $this->createTable('tbl_auth_assignment', array(
+            'itemname' => 'varchar(64) NOT NULL',
+            'userid' => 'int(11) NOT NULL',
+            'bizrule' => 'Text',
+            'data' => 'Text',
+            'PRIMARY KEY(itemname,userid)',
+                ), 'ENGINE=InnoDB');
+
+        // the tbl_auth_assignment.itemname is a referrence to tbl_auth_item.name
+        $this->addForeignkey('fk_auth_assignment_itemname', 'tbl_auth_assignment', 'itemname', 'tbl_auth_item', 'name', 'CASCADE', 'CASCADE');
+
+        // the tbl_auth_assignment.userid is a referrence to tbl_user.id
+        $this->addForeignkey('fk_auth_assignment_userid', 'tbl_auth_assignment', 'userid', 'tbl_user', 'id', 'CASCADE', 'RESTRICT'); // ต้องใส่ RESTRICT ในตำราใส่เป็น CASCADE ซึ่ง RUN ไม่ผ่าน
+    }
+
+    public function down() {
+//		echo "m130710_055925_create_rbac_tables does not support migration down.\n";
+//		return false;
+        $this->truncateTable('tbl_auth_assignment');
+        $this->truncateTable('tbl_auth_item_child');
+        $this->truncateTable('tbl_auth_item');
+        $this->dropTable('tbl_auth_assignment');
+        $this->dropTable('tbl_auth_item_child');
+        $this->dropTable('tbl_auth_item');
+    }
+
+    /*
+      // Use safeUp/safeDown to do migration with transaction
+      public function safeUp()
+      {
+      }
+
+      public function safeDown()
+      {
+      }
+     */
+}

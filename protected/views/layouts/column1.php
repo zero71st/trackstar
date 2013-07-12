a695c5046b8ac47124bd521e30601863ddc179d2
MisX7 <MisX7@Mis17>
2013-07-12 20:34:08 +0700

    Guest user can not access admin module have done!

--- protected/views/layouts/column1.php
+++ protected/views/layouts/column1.php
@@ -1,5 +1,5 @@
 <?php /* @var $this Controller */ ?>
-<?php $this->beginContent('//layouts/main'); ?> // Double slash (//) ให้ค้นหาภายใต้ Folder view เท่านั้น ถ้าไม่ระบุ Yii จะหาทั้ง Module
+<?php $this->beginContent('//layouts/main'); // Double slash (//) ให้ค้นหาภายใต้ Folder view เท่านั้น ถ้าไม่ระบุ Yii จะหาทั้ง Module ?>
 <div id="content">
     <?php echo $content; ?>
 </div><!-- content -->
